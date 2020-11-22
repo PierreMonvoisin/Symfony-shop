b2773d56b86add69f695918f3d53001ed24d72ed
PierreMonvoisin <monvoisin.pierre@gmail.com>
2020-11-22 11:59:52 +0100

    First push with two simple template twig and one controller

---
+++ src/Controller/MainController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(): Response
+    {
+        return $this->render('main/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    /**
+     * @Route("/shop", name="shop")
+     */
+    public function home(){
+        return $this->render('main/shop.html.twig');
+    }
+}
