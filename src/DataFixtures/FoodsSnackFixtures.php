0a9e4133e556f3f9bfc9f1e8ec2411fe62def3e7
PierreMonvoisin <monvoisin.pierre@gmail.com>
2020-11-27 14:35:21 +0100

    Fixtures done

--- src/DataFixtures/FoodsSnackFixtures.php
+++ src/DataFixtures/FoodsSnackFixtures.php
@@ -4,21 +4,21 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-use App\Entity\DrinksSoda;
+use App\Entity\FoodsSnack;
 
-class DrinksSodaFixtures extends Fixture
+class FoodsSnackFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
         for ($i = 0; $i <= 10; $i++){
-            $drinksSoda = new DrinksSoda();
+            $foodsSnack = new FoodsSnack();
 
-            $drinksSoda->setTitle("Titre de l'article $i")
+            $foodsSnack->setTitle("Titre de l'article $i")
                         ->setDescription("<p>Ceci est la description numéro $i du même article.</p>")
-                        ->setPrice(rand(3,20))
+                        ->setPrice(rand(4,20))
                         ->setImage("http://placehold.it/300x300");
 
-            $manager->persist($drinksSoda);
+            $manager->persist($foodsSnack);
         }
         $manager->flush();
     }
